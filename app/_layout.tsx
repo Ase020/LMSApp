4473ecdab4c9d45958ba12b8d5b7c091daca4e23
felixNyalenda <felixasejunior@gmail.com>
2024-11-21 00:14:18 +0300

    Slider build

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Stack } from "expo-router";
 import ThemeProvider from "./context/theme-context";
+import { Platform, StatusBar } from "react-native";
 
 export default function _layout() {
   return (
     <ThemeProvider>
+      {Platform.OS === "android" ? (
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="#transparent"
+          translucent={false}
+        />
+      ) : (
+        <StatusBar barStyle="dark-content" />
+      )}
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="(routes)/onboarding/index" />
--- screens/onboarding.screen.tsx
+++ screens/onboarding.screen.tsx
@@ -1,12 +1,53 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import { onBoardingSlides } from "@/configs/constants";
+import Slider from "@/components/onboarding/slider";
+import Slide from "@/components/onboarding/slide";
 
 export default function OnboardingScreen() {
+  const [index, setIndex] = React.useState(0);
+  const prev = onBoardingSlides[index - 1];
+  const next = onBoardingSlides[index + 1];
+
   return (
-    <View>
-      <Text>OnboardingScreen</Text>
-    </View>
+    <GestureHandlerRootView
+      style={{
+        flex: 1,
+      }}
+    >
+      <Slider
+        key={index}
+        index={index}
+        setIndex={setIndex}
+        prev={
+          prev && (
+            <Slide
+              index={index}
+              setIndex={setIndex}
+              slide={prev}
+              totalSlides={onBoardingSlides.length}
+            />
+          )
+        }
+        next={
+          next && (
+            <Slide
+              index={index}
+              setIndex={setIndex}
+              slide={next}
+              totalSlides={onBoardingSlides.length}
+            />
+          )
+        }
+      >
+        <Slide
+          index={index}
+          setIndex={setIndex}
+          slide={onBoardingSlides[index]}
+          totalSlides={onBoardingSlides.length}
+        />
+      </Slider>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({});
